30bc76902cf8c47bf2ffeacd88f4faa91f4e865e
matejslovak <56106320+matejslovak@users.noreply.github.com>
2019-10-13 18:31:49 +0200

    Open_Lab_02._3

--- Open-Lab-02.3/Program.cs
+++ Open-Lab-02.3/Program.cs
@@ -6,7 +6,14 @@ namespace Open_Lab_02._3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter 2 numbers");
+            string firstnumber = Console.ReadLine();
+            string secondnumber = Console.ReadLine();
+            double a = Convert.ToInt32(firstnumber);
+            double b = Convert.ToInt32(secondnumber);
+            double c = a % b;
+            Console.WriteLine(c);
+
         }
     }
 }
